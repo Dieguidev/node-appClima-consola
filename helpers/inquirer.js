aa292d2a7326aa1df4e66f05f8783c2b318d363d
Dieguidev <diegogaraycullas@gmail.com>
2024-02-11 03:57:41 -0500

    first commit -m

---
+++ helpers/inquirer.js
@@ -0,0 +1,155 @@
+require("colors");
+const inquirer = require("inquirer");
+
+const questions = [
+  {
+    type: "list",
+    name: "opcion",
+    message: "¿Que desea hacer?",
+    choices: [
+      {
+        value: "1",
+        name: `${"1.".green} Crear tarea`,
+      },
+      {
+        value: "2",
+        name: `${"2.".green} Listar tareas`,
+      },
+      {
+        value: "3",
+        name: `${"3.".green} Listar tareas completadas`,
+      },
+      {
+        value: "4",
+        name: `${"4.".green} Listar tareas pendientes`,
+      },
+      {
+        value: "5",
+        name: `${"5.".green} Completar tarea(s)`,
+      },
+      {
+        value: "6",
+        name: `${"6.".green} Borrar tarea`,
+      },
+      {
+        value: "0",
+        name: `${"0.".green} Salir`,
+      },
+    ],
+  },
+];
+
+const inquireMenu = async () => {
+  // console.clear();
+  console.log("=============================".green);
+  console.log(`   Seleccione una opcion`.white);
+  console.log("=============================\n".green);
+
+  const { opcion } = await inquirer.prompt(questions);
+
+  return opcion;
+};
+
+const pausa = async () => {
+  const question = [
+    {
+      type: "input",
+      name: "enter",
+      message: `Presione ${"enter".green} para continuar`,
+    },
+  ];
+
+  console.log("\n");
+  await inquirer.prompt(question); // Espera a que el usuario presione enter para continuar
+  // console.log("\n"); // Imprime un salto de línea para separar la salida de la entrada del usuario
+  // return; // Retorna undefined para que el método pueda ser utilizado como una función de flecha
+};
+
+const leerInput = async (message) => {
+  const question = [
+    {
+      type: "input",
+      name: "desc",
+      message,
+      validate(value) {
+        if (value.length === 0) {
+          return "Por favor ingrese un valor";
+        }
+        return true;
+      },
+    },
+  ];
+
+  const { desc } = await inquirer.prompt(question);
+  return desc; // Retorna el valor ingresado por el usuario en la variable desc
+};
+
+const mostrarListadoChecklist = async (tareas = []) => {
+  const choices = tareas.map((tarea, i) => {
+    const idx = `${i + 1}.`.green;
+    return {
+      value: tarea.id,
+      name: `${idx} ${tarea.desc}`,
+      checked: tarea.completadoEn ? true : false, // Si completadoEn es truthy, se marca como completado, de lo contrario, como pendiente.
+    };
+  });
+
+  const preguntas = [
+    {
+      type: "checkbox",
+      name: "ids",
+      message: "Selecciones",
+      choices,
+    },
+  ];
+  const { ids } = await inquirer.prompt(preguntas);
+  return ids;
+};
+
+const listadoTareasBorrar = async (tareas = []) => {
+  const choices = tareas.map((tarea, i) => {
+    const idx = `${i + 1}.`.green;
+    return {
+      value: tarea.id,
+      name: `${idx} ${tarea.desc}`,
+    };
+  });
+
+  choices.unshift({
+    value: "0",
+    name: "0.".green + " Cancelar",
+  });
+
+  const preguntas = [
+    {
+      type: "list",
+      name: "id",
+      message: "Borrar",
+      choices,
+    },
+  ];
+  const { id } = await inquirer.prompt(preguntas);
+  return id;
+};
+
+const confirmar = async (message) => {
+  const question = [
+    {
+      type: "confirm",
+      name: "ok",
+      message,
+    },
+  ];
+
+  const { ok } = await inquirer.prompt(question);
+  return ok;
+};
+
+module.exports = {
+  inquireMenu,
+  pausa,
+  leerInput,
+  listadoTareasBorrar,
+  confirmar,
+  mostrarListadoChecklist,
+};
