1be3ea065c2b485670f87e075bce0f5bfe9a5161
Dieguidev <diegogaraycullas@gmail.com>
2024-02-15 08:06:31 -0500

    feat: Integrar solicitud HTTP a API de Mapbox en Searches.city
    - Modificado el método city para realizar una solicitud HTTP a la API de Mapbox utilizando axios
    - Se utiliza la URL base de la API de Mapbox junto con el nombre de la ciudad proporcionado por el usuario para obtener información geográfica
    - Se incluyen parámetros de consulta para definir la proximidad, el idioma y el límite de resultados de la búsqueda
    - Se muestra por consola la respuesta de la solicitud HTTP, que contiene datos sobre la ubicación de la ciudad
    - Agregado manejo de errores para la solicitud HTTP

--- models/searches.js
+++ models/searches.js
@@ -7,12 +7,25 @@ class Searches {
     //todo: read db if exists
   }
 
+  get paramsMapbox() {
+    return {
+      proximity: 'ip',
+      language: 'es',
+      limit: 5,
+      access_token:
+        'pk.eyJ1IjoiZGllZ3VpZGV2IiwiYSI6ImNsc243Z29rNjAxOGMyaW12dGN0NW93aWUifQ.lAdrVMV_I3K0f0zB9X8PXA',
+    };
+  }
+
   async city(place = '') {
     try {
       //todo: peticion http
-      // console.log('ciudad', place);
-      const resp = await axios.get('https://reqres.in/api/users?page=2');
-      console.log(resp.data.per_page);
+      const instance = axios.create({
+        baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json`,
+        params: this.paramsMapbox,
+      });
+      const resp = await instance.get();
+      console.log(resp.data);
       return [];
     } catch (error) {
       return [];
